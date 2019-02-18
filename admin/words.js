aad522ed3c7abf3ef6f4ceab3ad36628785ca6a2
Pix <jens@iMac.local>
2019-02-18 16:46:44 +0100

    2.0.0 settings window material design

--- admin/words.js
+++ admin/words.js
@@ -4,48 +4,123 @@
 systemDictionary = {
     "tankerkoenig Adapter settings": {
         "en": "tankerkoenig Adapter settings",
-        "de": "tankerkoenig Adapter Einstellungen",
-        "ru": "Настройки tankerkoenig Adapter"
+        "de": "tankerkoenig Adaptereinstellungen",
+        "ru": "tankerkoenig Настройки адаптера",
+        "pt": "Configurações do adaptador tankerkoenig",
+        "nl": "tankerkoenig Adapterinstellingen",
+        "fr": "tankerkoenig Paramètres de l'adaptateur",
+        "it": "tankerkoenig Impostazioni dell'adattatore",
+        "es": "configuración del adaptador tankerkoenig",
+        "pl": "tankerkoenig Ustawienia adaptera",
+        "zh-cn": "tankerkoenig适配器设置"
     },
     "tankerkoenig API": {
-        "en": "tankerkoenig api key (36 digits):",
-        "de": "tankerkoenig API-Schlüssel (36 Zeichen):",
-        "ru": "tankerkoenig API-Key:"
+        "en": "tankerkoenig API key (36 digits)",
+        "de": "tankerkoenig API-Schlüssel (36 Ziffern)",
+        "ru": "API-ключ tankerkoenig (36 цифр)",
+        "pt": "chave de API tankerkoenig (36 dígitos)",
+        "nl": "tankerkoenig API-sleutel (36 cijfers)",
+        "fr": "clé API tankerkoenig (36 chiffres)",
+        "it": "Chiave API tankerkoenig (36 cifre)",
+        "es": "clave API de tankerkoenig (36 dígitos)",
+        "pl": "klucz API tankerkoenig (36 cyfr)",
+        "zh-cn": "tankerkoenig API密钥（36位）"
     },
     "API-Key": {
-        "en": "api key",
+        "en": "API key",
         "de": "API-Schlüssel",
-        "ru": "API-Key"
+        "ru": "Ключ API",
+        "pt": "Chave API",
+        "nl": "API sleutel",
+        "fr": "Clé API",
+        "it": "Chiave API",
+        "es": "Clave API",
+        "pl": "Klucz API",
+        "zh-cn": "API密钥"
     },
     "Tankstellen":       	{
-        "en": "Gas Stations",
+        "en": "gas stations",
         "de": "Tankstellen",
-        "ru": "Tankstellen"
+        "ru": "автозаправки",
+        "pt": "Posto de gasolina",
+        "nl": "benzine station",
+        "fr": "les stations-service",
+        "it": "stazioni di servizio",
+        "es": "gasolineras",
+        "pl": "stacje benzynowe",
+        "zh-cn": "加油站"
     },
     "Geben Sie die ID der gew&uuml;nschten Tankstelle ein. Es sind maximal zehn Tankestellen m&ouml;glich.": {
-        "en": "ID of gas station",
-        "de": "Geben Sie die ID der gew&uuml;nschten Tankstelle ein. Es sind maximal zehn Tankestellen m&ouml;glich.<br>Sie erhalten die ID der Tankstellen durch Eingabe Ihrer Geo-Koordinaten bei der Umkreissuche auf tankerkoenig.de",
-        "ru": "ID of gas station"
+        "en": "Enter the ID of the required gas station. A maximum of ten fueling stations are possible.",
+        "de": "Geben Sie die ID der gewünschten Tankstelle ein. Maximal zehn Tankstellen sind möglich.",
+        "ru": "Введите идентификатор нужной заправки. Возможны максимум десять заправочных станций.",
+        "pt": "Digite o ID do posto de gasolina necessário. Um máximo de dez estações de abastecimento são possíveis.",
+        "nl": "Voer de ID van het vereiste tankstation in. Er zijn maximaal tien tankstations mogelijk.",
+        "fr": "Entrez l'ID de la station-service requise. Un maximum de dix stations-service est possible.",
+        "it": "Inserire l'ID della stazione di servizio richiesta. Sono possibili un massimo di dieci stazioni di rifornimento.",
+        "es": "Introduzca la ID de la gasolinera requerida. Un máximo de diez estaciones de combustible son posibles.",
+        "pl": "Wprowadź identyfikator wymaganej stacji benzynowej. Możliwe jest maksymalnie dziesięć stacji paliw.",
+        "zh-cn": "输入所需加油站的ID。最多可以有十个加油站。"
     },
     "Sie erhalten den API Schl&uuml;ssel auf der Seite creativecommons.tankerkoenig.de": {
-        "en": "Input your personal 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "en": "Input your personal 36 digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
         "de": "Sie erhalten den API Schl&uuml;ssel auf der Seite <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
-        "ru": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>"
+        "ru": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "pt": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "nl": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "fr": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "it": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "es": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "pl": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>",
+        "zh-cn": "Input the 36digit API-Key from <a href='https://creativecommons.tankerkoenig.de/#register' target='_blank' alt='register API' >tankerkoenig.de</a>"
     },
     "ID Tankstelle": {
         "en": "Station ID (36 digits)",
-        "de": "ID Tankstelle (36 Zeichen)",
-        "ru": "ID Tankstelle (36 digits)"
+        "de": "Stations-ID (36 Ziffern)",
+        "ru": "Идентификатор станции (36 цифр)",
+        "pt": "ID da estação (36 dígitos)",
+        "nl": "Stations-ID (36 cijfers)",
+        "fr": "Identifiant de la station (36 chiffres)",
+        "it": "ID della stazione (36 cifre)",
+        "es": "ID de estación (36 dígitos)",
+        "pl": "Identyfikator stacji (36 cyfr)",
+        "zh-cn": "站ID（36位）"
     },
     "Name Tankstelle": {
-        "en": "Custom station name",
-        "de": "Eigener Name für Tankstelle",
-        "ru": "Custom station name"
+        "en": "Custom name for station",
+        "de": "Eignener Name für die Station",
+        "ru": "Пользовательское имя для станции",
+        "pt": "Nome personalizado para a estação",
+        "nl": "Aangepaste naam voor station",
+        "fr": "Nom personnalisé pour la station",
+        "it": "Nome personalizzato per la stazione",
+        "es": "Nombre personalizado para la estación",
+        "pl": "Niestandardowa nazwa dla stacji",
+        "zh-cn": "车站的自定义名称"
     },
     "Nummer": {
         "en": "No.",
         "de": "Nr.",
-        "ru": "#"
+        "ru": "#",
+        "pt": "#",
+        "nl": "#",
+        "fr": "#",
+        "it": "#",
+        "es": "#",
+        "pl": "#",
+        "zh-cn": "#"
+    },
+    "StationID falsch oder leer": {
+        "en": "StationID: wrong format or missing",
+        "de": "StationID fehlt oder falsches Format",
+        "ru": "StationID: неверный формат или отсутствует",
+        "pt": "StationID: formato errado ou ausente",
+        "nl": "StationID: verkeerd formaat of ontbreekt",
+        "fr": "StationID: format incorrect ou manquant",
+        "it": "ID stazione: formato errato o mancante",
+        "es": "StationID: formato incorrecto o falta",
+        "pl": "StationID: zły format lub brak",
+        "zh-cn": "StationID：格式错误或丢失"
     },
     "Werte nullen": {
         "en": "Reset values",
@@ -63,9 +138,16 @@ systemDictionary = {
         "ru": "Werte vor jeder Aktualisierung nullen (übersetzen)"
     },
     "kein Log": {
-        "en": "Minimize log events",
+        "en": "reduce log events",
         "de": "Log Ausgaben reduzieren",
-        "ru": "Minimize log events (übersetzen)"
+        "ru": "уменьшить журнал событий",
+        "pt": "reduzir eventos de log",
+        "nl": "log gebeurtenissen verminderen",
+        "fr": "réduire les événements de journal",
+        "it": "ridurre gli eventi del registro",
+        "es": "reducir los eventos de registro",
+        "pl": "ogranicz dzienniki zdarzeń",
+        "zh-cn": "减少日志事件"
     },
     "Logdaten minimieren": {
         "en": "Logging data",
@@ -73,28 +155,75 @@ systemDictionary = {
         "ru": "Log Daten (übersetzen)"
     },
     "Logdaten ...": {
-        "en": "Every run creates log entries. Wwiting log events can be opted out here.",
+        "en": "Each update updates log output. To minimize the number of write accesses, the output can be reduced by a tick.",
         "de": "Bei jeder Aktualisierung werden Log-Ausgaben geschrieben. Um die Zahl der Schreibzugriffe zu minimieren, kann hier durch einen Haken die Ausgabe reduziert werden.",
-        "ru": "Every run creates log entries. Wwiting log events can be opted out here. (übersetzen)"
+        "ru": "Каждое обновление обновляет вывод журнала. Чтобы свести к минимуму количество обращений к записи, вывод можно уменьшить с помощью галочки.",
+        "pt": "Cada atualização atualiza a saída do log. Para minimizar o número de acessos de gravação, a saída pode ser reduzida por um tick.",
+        "nl": "Bij elke update wordt de logboekuitvoer bijgewerkt. Om het aantal schrijftoegangen te minimaliseren, kan de uitvoer worden verminderd met een vinkje.",
+        "fr": "Chaque mise à jour met à jour la sortie du journal. Pour minimiser le nombre d'accès en écriture, la sortie peut être réduite par une coche.",
+        "it": "Ogni aggiornamento aggiorna l'output del registro. Per minimizzare il numero di accessi in scrittura, l'output può essere ridotto di un tick.",
+        "es": "Cada actualización actualiza la salida del registro. Para minimizar el número de accesos de escritura, la salida se puede reducir en una marca.",
+        "pl": "Każda aktualizacja aktualizuje dane wyjściowe dziennika. Aby zminimalizować liczbę dostępów zapisu, dane wyjściowe można zmniejszyć za pomocą haczyka.",
+        "zh-cn": "每次更新都会更新日志输出为了最大限度地减少写访问次数，输出可以减少一个滴答。"
     },
     "Intervall": {
-        "en": "Interval",
+        "en": "interval",
         "de": "Intervall",
-        "ru": "Interval"
+        "ru": "интервал",
+        "pt": "intervalo",
+        "nl": "interval",
+        "fr": "intervalle",
+        "it": "intervallo",
+        "es": "intervalo",
+        "pl": "interwał",
+        "zh-cn": "间隔"
     },
     "Abfrageintervall": {
         "en": "Time between requests",
-        "de": "Dauer zwischen den Abfragen",
-        "ru": "Time between requests (übersetzen)"
+        "de": "Zeit zwischen den Abfragen",
+        "ru": "Время между запросами",
+        "pt": "Tempo entre pedidos",
+        "nl": "Tijd tussen aanvragen",
+        "fr": "Temps entre les demandes",
+        "it": "Tempo tra le richieste",
+        "es": "Tiempo entre peticiones",
+        "pl": "Czas między żądaniami",
+        "zh-cn": "请求之间的时间"
     },
     "in Minuten": {
-        "en": "in minutes (at least 5min)",
-        "de": "In Minuten (min. 5min)",
-        "ru": "in minutes (at least 5min) (übersetzen)"
+        "en": "in minutes (at least 5)",
+        "de": "in Minuten (mindestens 5)",
+        "ru": "в минутах (не менее 5)",
+        "pt": "em minutos (pelo menos 5)",
+        "nl": "in minuten (minstens 5)",
+        "fr": "en minutes (au moins 5)",
+        "it": "in minuti (almeno 5)",
+        "es": "en minutos (al menos 5)",
+        "pl": "w ciągu kilku minut (co najmniej 5)",
+        "zh-cn": "在几分钟内（至少5）"
+    },
+    "Abfrageinfo": {
+        "en": "The data of the source feed are updated by the server at tankerkoenig.de only every 4 minutes. The shortest polling interval is therefore 5 minutes because a more frequent polling makes no sense.",
+        "de": "Die Daten des Quellfeeds werden vom Server bei tankerkoenig.de nur alle 4 Minuten aktualisiert. Das kürzeste Abfrageintervall beträgt daher 5 Minuten, da eine häufigere Abfrage keinen Sinn macht.",
+        "ru": "Данные исходного канала обновляются сервером по адресу tankerkoenig.de только каждые 4 минуты. Поэтому самый короткий интервал опроса составляет 5 минут, потому что более частый опрос не имеет смысла.",
+        "pt": "Os dados do feed de origem são atualizados pelo servidor em tankerkoenig.de somente a cada 4 minutos. O menor intervalo de pesquisa é, portanto, de 5 minutos, porque uma pesquisa mais freqüente não faz sentido.",
+        "nl": "De gegevens van de bronfeed worden slechts om de vier minuten bijgewerkt door de server op tankerkoenig.de. Het kortste polling-interval is daarom 5 minuten omdat een frequentere polling geen zin heeft.",
+        "fr": "Les données du flux source ne sont mises à jour par le serveur sur tankerkoenig.de que toutes les 4 minutes. L'intervalle d'interrogation le plus court est donc de 5 minutes, car une interrogation plus fréquente n'a aucun sens.",
+        "it": "I dati del feed di origine vengono aggiornati dal server su tankerkoenig.de solo ogni 4 minuti. L'intervallo di polling più breve è quindi di 5 minuti perché un sondaggio più frequente non ha senso.",
+        "es": "El servidor de tankerkoenig.de actualiza los datos de la fuente de alimentación solo cada 4 minutos. Por lo tanto, el intervalo de sondeo más corto es de 5 minutos porque un sondeo más frecuente no tiene sentido.",
+        "pl": "Dane z kanału źródłowego są aktualizowane przez serwer na tankerkoenig.de tylko co 4 minuty. Najkrótszy interwał sondowania wynosi zatem 5 minut, ponieważ częstsze sondowanie nie ma sensu.",
+        "zh-cn": "服务器在tankerkoenig.de仅每4分钟更新一次源数据。因此，最短的轮询间隔是5分钟，因为更频繁的轮询是没有意义的。"
     },
     "Settings": {
         "en": "settings",
         "de": "Einstellungen",
-        "ru": "settings"
+        "ru": "настройки",
+        "pt": "definições",
+        "nl": "instellingen",
+        "fr": "réglages",
+        "it": "impostazioni",
+        "es": "ajustes",
+        "pl": "ustawienia",
+        "zh-cn": "设置"
     }
 };
