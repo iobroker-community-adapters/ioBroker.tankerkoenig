621dda5963f2a24be2a5e9dd32b35d91c98cbab5
Ingo Fischer <github@fischer-ka.de>
2019-03-03 22:45:10 +0100

    add adapter-core

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 /*jslint node: true */
 
 'use strict';
-const utils       = require(__dirname + '/lib/utils'); // Get common adapter utils
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
 const request     = require('request');
 
 const adapterName = require('./package.json').name;
