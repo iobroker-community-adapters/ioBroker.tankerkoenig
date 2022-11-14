b8dc72ce5e6c784b7ebaff93d578ac095d8603a2
Issi <issi.dev.iobroker@gmail.com>
2022-11-14 03:17:52 +0100

    add checkValidConfig function
    the function checks if ID and name are set and if the ID already exists

--- admin/src/Modal/AddModal.tsx
+++ admin/src/Modal/AddModal.tsx
@@ -1,6 +1,15 @@
-import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from '@mui/material';
+import {
+	Alert,
+	AlertTitle,
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Grid,
+} from '@mui/material';
 import { useI18n } from 'iobroker-react/hooks';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { AddStationDialog } from '../component/AddStationDialog';
 import { StationFinder } from '../component/StationFinder';
 
@@ -10,9 +19,18 @@ export interface AddModalProps {
 	open: boolean;
 	onClose: () => void;
 }
-
-export const AddModal: React.FC<AddModalProps> = ({ newRow, open, onClose }): JSX.Element => {
+let resetTimeout: NodeJS.Timeout;
+export const AddModal: React.FC<AddModalProps> = ({ newRow, open, onClose, currentRows }): JSX.Element => {
 	const [row, setRow] = useState<ioBroker.Station>();
+	const [validConfig, setValidConfig] = useState<{
+		valid: boolean;
+		message: string;
+		alert: boolean;
+	}>({
+		valid: false,
+		alert: false,
+		message: '',
+	});
 
 	const { translate: _ } = useI18n();
 
@@ -26,6 +44,46 @@ export const AddModal: React.FC<AddModalProps> = ({ newRow, open, onClose }): JS
 	const handleClose = async (): Promise<void> => {
 		onClose();
 	};
+
+	// check if in the current row id and name are set  and if the id and name is not already in the list
+	const checkValidConfig = (row: ioBroker.Station) => {
+		const max = 36;
+		const pattern = /[0-9|a-z]{8}\-[0-9|a-z]{4}\-[0-9|a-z]{4}\-[0-9|a-z]{4}\-[0-9|a-z]{12}/g;
+		// check if the id is a valid uuid
+		if (row.station && row.station.match(pattern) && row.station.length === max) {
+			if (row.stationname && row.stationname.length > 0) {
+				// check if the id is not already in the list
+				if (currentRows.find((element) => element.station === row.station)) {
+					// id is already in the list
+					setValidConfig({
+						...validConfig,
+						valid: false,
+						alert: true,
+						message: _('id already in list'),
+					});
+				} else {
+					// id is not in the list
+					setValidConfig({ ...validConfig, valid: true, alert: false, message: '' });
+				}
+			} else {
+				// name is not set
+				setValidConfig({ ...validConfig, valid: false, alert: true, message: _('name is empty') });
+			}
+		} else {
+			// id is not a valid uuid
+			setValidConfig({ ...validConfig, valid: false, alert: true, message: _('id not valid') });
+		}
+	};
+
+	// reset validConfig by open the modal
+	useEffect(() => {
+		if (resetTimeout) clearTimeout(resetTimeout);
+		resetTimeout = setTimeout(() => {
+			if (open) {
+				setValidConfig({ ...validConfig, valid: false, alert: false, message: '' });
+			}
+		}, 10);
+	}, [open]);
 
 	return (
 		<React.Fragment>
@@ -56,14 +114,27 @@ export const AddModal: React.FC<AddModalProps> = ({ newRow, open, onClose }): JS
 						justifyContent: 'center',
 					}}
 				>
+					{validConfig.alert && !validConfig.valid ? (
+						<Alert severity="warning">
+							<AlertTitle>Warning</AlertTitle>
+							{validConfig.message}
+						</Alert>
+					) : null}
 					<Grid container spacing={1}>
-						<AddStationDialog addRow={(value) => setRow(value)} />
+						<AddStationDialog
+							addRow={(value) => {
+								setRow(value);
+								checkValidConfig(value);
+							}}
+						/>
 						<StationFinder />
 					</Grid>
 				</DialogContent>
 
 				<DialogActions>
-					<Button onClick={handleClickAdd}>{_('add')}</Button>
+					<Button disabled={!validConfig.valid} onClick={handleClickAdd}>
+						{_('add')}
+					</Button>
 					<Button onClick={handleClose}>{_('cancel')}</Button>
 				</DialogActions>
 			</Dialog>
